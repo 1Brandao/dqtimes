c8d341c8e5091d8da325d210cb1241c0fb3357f3
WWexe <oeningvinicius@gmail.com>
2025-11-26 01:33:59 -0300

    finality task #62 and #63, forecasting/previsao works

--- dqtimes/app/tasks.py
+++ dqtimes/app/tasks.py
@@ -2,10 +2,14 @@ import os
 import time
 from celery import Celery
 from datetime import datetime
+from datetime import timezone
 from celery.signals import task_prerun, task_success, task_failure
 from dotenv import load_dotenv
 load_dotenv()
 from db import SessionLocal, Job
+
+#package de previsao - task #63
+from previsao import run_prediction_model
 
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
 
@@ -22,7 +26,7 @@ celery_app.conf.update(
 @celery_app.task(name="predict", bind=True)
 def predict(self, payload):
     """
-    Worker para previsões
+    Worker para previsoes
     Espera um payload: {
         "series": [10, 20, 30...],
         "name_ref": "nome-modelo || uid"
@@ -34,25 +38,25 @@ def predict(self, payload):
     periods = payload.get('periods')
     
     if not series or not isinstance(series, list):
-        raise ValueError("O parametro 'series' e obrigatorio e deve ser um Array")
+        raise ValueError("O parametro 'series' e obrigatorio e deve ser uma LIST")
+       
     
     if not periods or not isinstance(periods, int):
-        raise ValueError("O parametro 'periods' e obrigatorio e deve ser um Int")
+        raise ValueError("O parametro 'periods' e obrigatorio e deve ser um INT")
     
-    print(f"[{self.request.id}] Iniciando previsão para '{name_ref}' com {periods} periodos -")
+    print(f"[{self.request.id}] Delegando previsão para o modulo previsao.py---")
     
-    #Simula processamento para testar o worker consumindo a fila
-    time.sleep(5)
+    time.sleep(2)
+    result_series = run_prediction_model(series, periods)
     
-    #Mock resultado
-    prediction_mock = [x * 1.05 for x in series[-periods:]]
-    
+    #retorno formatado
     return {
         "name_ref": name_ref,
         "input_series_len": len(series),
-        "prediction": prediction_mock,
-        "processed_at": datetime.utcnow().isoformat()
+        "prediction": result_series,
+        "processed_at": datetime.now(timezone.utc).isoformat()
     }
+    
 
 @task_prerun.connect
 def on_task_start(sender=None, task_id=None, **kwargs):
@@ -62,22 +66,29 @@ def on_task_start(sender=None, task_id=None, **kwargs):
             job = session.query(Job).filter_by(task_id=task_id).first()
             if job:
                 job.status = "IN_PROGRESS"
-                job.started_at = datetime.utcnow()
+                job.started_at = datetime.now(timezone.utc)
                 session.commit()
     except Exception as e:
         print(f"Erro ao atualziar status IN_PROGRESS: {e}")
 
+
 @task_success.connect
 def on_task_success(sender=None, result=None, task_id=None, **kwargs):
+    if not task_id and sender and hasattr(sender, 'request'):
+        task_id = sender.request.id
+        
     print(f"-- Sucesso Task ID: {task_id}")
+    
     try:
         with SessionLocal() as session:
             job = session.query(Job).filter_by(task_id=task_id).first()
             if job:
                 job.status = "SUCCESS"
-                job.finished_at = datetime.utcnow()
+                job.finished_at = datetime.now(timezone.utc)
                 job.result = result
                 session.commit()
+            else: 
+                print(f"ALERT: Job {task_id} não encontrado no banco para atualizar")
     except Exception as e:
         print(f"Erro ao atualizar status SUCCESS: {e}")
 
@@ -89,7 +100,7 @@ def on_task_failure(sender=None, task_id=None, exception=None, **kwargs):
             job = session.query(Job).filter_by(task_id=task_id).first()
             if job:
                 job.status = "FAILURE"
-                job.finished_at = datetime.utcnow()
+                job.finished_at = datetime.now(timezone.utc)
                 job.result = {"error": str(exception)}
                 session.commit()
     except Exception as e:
